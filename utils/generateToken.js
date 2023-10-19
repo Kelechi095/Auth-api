d0f982439127571af15194ed4d73342869fad4cb
Awoms Kelechi <awomskelechi@gmail.com>
2023-10-19 22:54:43 +0100

    refactored code to generate cookie

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -7,8 +7,8 @@ export const generateToken = (res, userID) => {
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: true /* process.env.NODE_ENV !== 'development' */,
-    sameSite: "none",
+    /* secure: true /*
+    sameSite: "none", */
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 };
