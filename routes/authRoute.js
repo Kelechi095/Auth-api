644b035217ff7d4527c6b568432e5375438b5342
Awoms Kelechi <awomskelechi@gmail.com>
2023-10-20 20:22:40 +0100

    refactor

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -10,8 +10,8 @@ router.route("/login").post(loginUser)
 router.route('/user').get(setUser)
 router.route('/google').post(loginWithGoogle)
 router.route('/logout').get(logoutUser)
-router.route("/products").get(authenticateUser, getProfile)
+router.route("/profile").get(authenticateUser, getProfile)
 router.route("/refresh").get(refresh)
-router.route("/update").post(updateUsername)
+router.route("/update").patch(updateUsername)
 
 export default router
